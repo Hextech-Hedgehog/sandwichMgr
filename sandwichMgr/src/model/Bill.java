2d0f08b77768e8d11ed4af1ef441ed912557157c
Hextech-Hedgehog <quentinart@hotmail.com>
2022-09-22 16:17:50 +0200

    Added Bill class

---
+++ sandwichMgr/src/model/Bill.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+public class Bill {
+
+    HashMap<LocalDate, Order> orders;
+
+    public Bill(HashMap<LocalDate, Order> orders) {
+        this.orders = orders;
+    }
+
+    public void addOrder(Order order) {
+        this.orders.put(order.getDate(), order);
+    }
+
+    public void addOrders(Order[] orders) {
+        this.orders.putAll(Arrays.stream(orders).collect(Collectors.toMap(Order::getDate, (o) -> o)));
+    }
+}
--- sandwichMgr/src/model/Order.java
+++ sandwichMgr/src/model/Order.java
@@ -1,14 +1,20 @@
 package model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
 
     private List<Sandwich> sandwiches = new ArrayList<>();
+    private LocalDate date = LocalDate.now();
 
     public List<Sandwich> getSandwiches() {
         return sandwiches;
+    }
+
+    public LocalDate getDate() {
+        return date;
     }
 
     public Order(Sandwich sandwich) {
@@ -22,5 +28,4 @@ public class Order {
     public void printOrderInfo() {
         this.sandwiches.forEach(Sandwich::printContents);
     }
-
 }
