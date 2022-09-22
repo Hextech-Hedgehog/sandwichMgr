8b1a2acbee7557769b7131d6649f96dade66acc9
Hextech-Hedgehog <quentinart@hotmail.com>
2022-09-22 15:34:28 +0200

    Added SandwichType enum and Ingredient class

---
+++ sandwichMgr/src/model/Ingredient.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Ingredient {
+
+    private String name;
+
+    public Ingredient(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
--- sandwichMgr/src/model/SandwichType.java
+++ sandwichMgr/src/model/SandwichType.java
@@ -1,4 +1,49 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum SandwichType {
+
+    HAM(new ArrayList<>(){{
+        add(new Ingredient("Bread"));
+        add(new Ingredient("Salad"));
+        add(new Ingredient("Ham"));
+        add(new Ingredient("Mayonnaise"));
+    }}),
+    ROAST_BEEF(new ArrayList<>(){{
+        add(new Ingredient("Bread"));
+        add(new Ingredient("Roasted beef"));
+        add(new Ingredient("Mayonnaise"));
+    }}),
+    SALAMI(new ArrayList<>(){{
+        add(new Ingredient("Bread"));
+        add(new Ingredient("Salami"));
+        add(new Ingredient("Mayonnaise"));
+    }}),
+    MEAT_BALL(new ArrayList<>(){{
+        add(new Ingredient("Bread"));
+        add(new Ingredient("Beef"));
+        add(new Ingredient("Mayonnaise"));
+    }}),
+    MARTINO(new ArrayList<>(){{
+        add(new Ingredient("Bread"));
+        add(new Ingredient("Beef"));
+        add(new Ingredient("Eggs"));
+        add(new Ingredient("Onions"));
+        add(new Ingredient("Tomatoes"));
+        add(new Ingredient("Pickles"));
+        add(new Ingredient("Martino sauce"));
+    }});
+
+    private List<Ingredient> ingredients = new ArrayList<>();
+
+    private SandwichType(List<Ingredient> ingredients) {
+            this.ingredients.addAll(ingredients);
+    }
+
+    public void printInfo() {
+        this.ingredients.forEach(System.out::print);
+    }
+
 }
