ed1a378233e432dd5418580d61249d48824bd458
Hextech-Hedgehog <quentinart@hotmail.com>
2022-11-28 15:10:59 +0100

    Added sandwich, order and bill JpaRepositories

--- sandwichMgr/src/main/java/sandwich/model/Shop.java
+++ sandwichMgr/src/main/java/sandwich/model/Shop.java
@@ -1,10 +1,10 @@
 package sandwich.model;
 
-public enum Shop {
+public enum Shop { //TODO change from enum to normal class
     PINKYS("sandwichTypePinkys.txt"),
     VLEUGELS("sandwichTypeVleugels.txt");
 
-    private final String pathWay;
+    private final String pathWay; //TODO remove
 
     Shop(String pathWay) {
         this.pathWay = pathWay;
---
+++ sandwichMgr/src/main/java/sandwich/repository/SandwichJpaRepository.java
@@ -0,0 +1,29 @@
+package sandwich.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import sandwich.model.Sandwich;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public interface SandwichJpaRepository extends JpaRepository {
+
+    @Modifying
+    @Query(value="insert into sandwich (club, butter, optional, s_uid, s_orid) values (:club, :butter, :rq, :u_id, :st_id)", nativeQuery = true)
+    void addSandwich(@Param("club") String isClub, @Param("butter") String hasButter, @Param("rq") String optionalRequirement,
+                     @Param("u_id") int userId, @Param("st_id") int sandwichTypeId);
+    @Query(value="select * from sandwich where sid=:id", nativeQuery = true)
+    Sandwich findSandwichById(@Param("id") int sandwichId);
+    @Query(value="select * from sandwich where s_uid=:id", nativeQuery = true)
+    List<Sandwich> findSandwichesByUser(@Param("id") int userId);
+    @Modifying
+    @Query(value="delete from sandwich where sid=:id", nativeQuery = true)
+    void removeSandwich(@Param("id") int sandwichId);
+    @Query(value="select * from sandwich", nativeQuery = true)
+    List<Sandwich> findSandwiches();
+}
